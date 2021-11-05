916d1a911255d410c4bc31f4b0b174b415dfbdf3
Enas <enaselmahdwi@gmail.com>
2021-11-04 20:33:24 -0600

    add folder solve all of read me quetions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, {useState } from 'react';
+import ProductContext from './contexts/ProductContext';
 import { Route } from 'react-router-dom';
 import data from './data';
+import CartContext from './contexts/CartContext';
 
 // Components
 import Navigation from './components/Navigation';
@@ -13,20 +15,26 @@ function App() {
 
 	const addItem = item => {
 		// add the given item to the cart
+		setCart([...cart,item])
 	};
 
 	return (
+		
 		<div className="App">
-			<Navigation cart={cart} />
+            <ProductContext.Provider value={{ products, addItem }}>
+		     <CartContext.Provider value={{cart}}>
+			<Navigation  />
 
 			{/* Routes */}
 			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
+				<Products  />
 			</Route>
 
 			<Route path="/cart">
-				<ShoppingCart cart={cart} />
+				<ShoppingCart  />
 			</Route>
+			</CartContext.Provider>
+			</ProductContext.Provider>
 		</div>
 	);
 }
